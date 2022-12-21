f0c0e5c1696b9067ad66611e0593c1b86d76c65c
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2022-12-21 17:50:36 +0200

    Freshen sync

--- sync.sh
+++ sync.sh
@@ -6,51 +6,23 @@ dotfiles_remote="git@github.com:dezgeg/dotfiles.git"
 secrets_remote="dezgeg@kapsi.fi:dotfiles/secrets"
 secrets_dir="$my_dir/secrets"
 
-uninstall=0
-dry_run=0
-skip_secrets=0
+flag_uninstall=0
+flag_dry_run=0
+flag_skip_secrets=0
 
 run_cmd() {
     echo "Running:" "$@"
-    if [ $dry_run = 0 ]; then
+    if [ $flag_dry_run = 0 ]; then
         "$@"
     fi
 }
 
-die() {
-    echo "$@"
-    exit 1
-}
+make_link() {
+    dotfiles_file=$1
+    home_file=$2
+    backup_file="$home_file.bak"
 
-for arg; do
-    case "$arg" in
-        "-d")
-            dry_run=1
-            shift;;
-        "-n")
-            skip_secrets=1
-            shift;;
-        "-u")
-            uninstall=1
-            skip_secrets=1
-            shift;;
-        *)
-            echo "Invalid arg: $arg"
-            exit 1;;
-    esac
-done
-
-for file in "$my_dir"/* "$my_dir"/.* ; do
-    file="$(basename "$file")"
-    case "$file" in
-        .|..|.git|.gitignore|share|secrets|sync.sh|.sw*|.*.sw*) continue;;
-    esac
-
-    home_file="$HOME/$file"
-    backup_file="$HOME/$file.bak"
-    dotfiles_file="$my_dir/$file"
-
-    if [ "$uninstall" = 1 ]; then
+    if [ "$flag_uninstall" = 1 ]; then
         if [ -L "$home_file" ]; then
             run_cmd rm "$home_file"
         fi
@@ -68,18 +40,55 @@ for file in "$my_dir"/* "$my_dir"/.* ; do
             run_cmd ln -s "$dotfiles_file" "$home_file"
         fi
     fi
+
+}
+
+die() {
+    echo "$@"
+    exit 1
+}
+
+for arg; do
+    case "$arg" in
+        "-d")
+            flag_dry_run=1
+            shift;;
+        "-n")
+            flag_skip_secrets=1
+            shift;;
+        "-u")
+            flag_uninstall=1
+            flag_skip_secrets=1
+            shift;;
+        *)
+            echo "Invalid arg: $arg"
+            exit 1;;
+    esac
+done
+
+for file in $(cd "$my_dir" && echo * .*) ; do
+    case "$file" in
+        .|..|.config|.git|.gitignore|share|secrets|sync.sh|vim|.sw*|.*.sw*|*.un~|.*.un~) continue;;
+    esac
+
+    make_link "$my_dir/$file" "$HOME/$file"
+done
+mkdir -p "$HOME/.config"
+for file in $(cd "$my_dir/.config" && echo *) ; do
+    make_link "$my_dir/.config/$file" "$HOME/.config/$file"
 done
 
 chmod 700 ~/.ssh      2>/dev/null
 chmod -R 600 ~/.ssh/* 2>/dev/null
 
-if [ $skip_secrets = 0 ]; then
+if [ $flag_skip_secrets = 0 ]; then
     if [ ! -d "$secrets_dir" ]; then
         run_cmd git clone "$secrets_remote" "$secrets_dir"
     else
-        pushd "$secrets_dir" >/dev/null
-        run_cmd git pull
-        popd >/dev/null
+        (
+            cd "$secrets_dir"
+            run_cmd git pull
+        )
     fi
     run_cmd chmod -R a-rw,u=rwX "$secrets_dir" >/dev/null
     echo
@@ -88,6 +97,5 @@ if [ $skip_secrets = 0 ]; then
     git remote rm origin
     git remote add origin "$dotfiles_remote"
     git fetch origin
-    git push -u origin master
     popd >/dev/null
 fi
